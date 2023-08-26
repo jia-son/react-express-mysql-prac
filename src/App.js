a8542d7b37cfa3d19d704a04714d7c807dd7c30c
jia-son <116625758+jia-son@users.noreply.github.com>
2023-08-26 23:01:27 +0900

    close #3

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import axios from 'axios';
 
 function App() {
+
+  const selectAll = async() => {
+    const res = await axios.get('/get');
+    console.log(res.data);
+  };
+
   return (
     <div className="App">
-      test
+      <h1>React-express-MySQl 연동 확인</h1>
+      <button onClick={selectAll}>전체 목록 조회</button>
     </div>
   );
 }

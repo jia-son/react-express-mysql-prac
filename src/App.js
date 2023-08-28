c60c7c8dd12c8ff61c27fb9a00973e901375db87
jia-son <116625758+jia-son@users.noreply.github.com>
2023-08-28 22:30:11 +0900

    게시글 상세 보기
    close #7

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { Route, Routes } from "react-router-dom";
 import NoticeViews from './components/notice/NoticeViews';
 import CreatePost from './components/notice/CreatePost';
+import DetailPost from './components/notice/DetailPost';
 import MainPage from './components/main/Main.js';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Route path='/' element={<MainPage />} />
         <Route path='/notice' element={<NoticeViews />} />
         <Route path='/createPost' element={<CreatePost />} />
+        <Route path='/detailPost/:postId' element={<DetailPost />} />
       </Routes>
     </>
   );
---
+++ src/components/notice/DetailPost.js
@@ -0,0 +1,66 @@
+import React, { useEffect, useState } from "react";
+import '../main/Main.js';
+import axios from 'axios';
+
+import { useNavigate, useParams } from "react-router-dom";
+
+function DetailPost() {
+
+    const navigate = useNavigate();
+
+    const [data, setData] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [blankNotice, setBlankNotice] = useState();
+    const { postId } = useParams();
+
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const response = await axios.get(`/post/${postId}`);
+                setData(response.data);
+            } catch (error) {
+                console.log('error', error);
+                const err = error.response;
+
+                if(err.status === 404 && err.data === '존재하지 않는 게시글입니다.') {
+                    setBlankNotice(err.data);
+                }
+            } finally {
+                setIsLoading(false);
+            }
+        }
+        fetchData();
+    }, [postId]);
+
+    return (
+        <>
+            <div className="mainBox">
+                <h1>디테일 페이지지렁이</h1>
+
+
+                <div>
+                    {isLoading ? (
+                        <p>Loading...</p>
+                    ) : (
+                        <>
+                            {blankNotice ? (
+                                <h2>{blankNotice}</h2>
+                            ) : (
+                                <>
+                                    <h3>{data.title}</h3>
+                                    <p>{data.content}</p>
+                                </>
+                            )}
+                        </>
+                    )}
+                </div>
+
+                <div>
+                    <button onClick={() => navigate('/notice')}>목록</button>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default DetailPost;

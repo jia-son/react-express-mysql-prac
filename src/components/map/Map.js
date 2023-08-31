bc3f27076203272f40994bd52de64ec3cad3d275
jia-son <116625758+jia-son@users.noreply.github.com>
2023-09-01 01:04:21 +0900

    검색어 입력받아 지도 출력하기 close #19
    close #19

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -6,35 +6,65 @@ const { kakao } = window;
 
 const Map = () => {
     const navigate = useNavigate();
-    
+    const [searchInfo, setSearchInfo] = useState();
+    const [map, setMap] = useState(null);
+
     const mapContainer = useRef(null);
-    const position = new kakao.maps.LatLng(33.450701, 126.570667);
     const mapOptions = {
-        center: position, // 지도의 중심좌표
-        level: 4 // 지도의 확대 레벨
+        center : new kakao.maps.LatLng(33.450701, 126.570667),
+        level : 3
     };
 
-    useEffect(() => {
-        const map = new kakao.maps.Map(mapContainer.current, mapOptions);
-        const marker = new kakao.maps.Marker({ position }); // 마커 생성
-        
-        // 커스텀 오버레이에 표출될 내용
-        // const content = `
-        //     <div class="customoverlay">
-        //       <span>포썸</span>
-        //     </div>`;
-      
-        // 커스텀 오버레이 생성
-        new kakao.maps.CustomOverlay({
-          map,
-          position,
-        //   content
-        });
-      
-        // 마커가 지도 위에 표시되도록 설정
-        marker.setMap(map);
-      }, []);
+    let infowindow = new kakao.maps.InfoWindow({zIndex:1});
 
+    useEffect(() => {
+        const newMap = new kakao.maps.Map(mapContainer.current, mapOptions);
+        setMap(newMap);
+    }, []);
+
+    function placesSearchCB (data, status, pagination) {
+        if (status === kakao.maps.services.Status.OK) {
+    
+            // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
+            // LatLngBounds 객체에 좌표를 추가합니다
+            var bounds = new kakao.maps.LatLngBounds();
+    
+            for (var i=0; i<data.length; i++) {
+                displayMarker(data[i]);    
+                bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
+            }       
+    
+            // 검색된 장소 위치를 기준으로 지도 범위를 재설정합니다
+            map.setBounds(bounds);
+        } 
+    }
+
+    function displayMarker(place) {
+    
+        // 마커를 생성하고 지도에 표시합니다
+        var marker = new kakao.maps.Marker({
+            map: map,
+            position: new kakao.maps.LatLng(place.y, place.x) 
+        });
+    
+        // 마커에 클릭이벤트를 등록합니다
+        kakao.maps.event.addListener(marker, 'click', function() {
+            // 마커를 클릭하면 장소명이 인포윈도우에 표출됩니다
+            infowindow.setContent('<div style="padding:5px;font-size:12px;">' + place.place_name + '</div>');
+            infowindow.open(map, marker);
+        });
+    }
+
+    const onChangeHandler = (e) => {
+        setSearchInfo((info) => ({ ...info, [e.target.name]: e.target.value }));
+    };
+
+    const onSubmitHandler = (e) => {
+        e.preventDefault();
+        const ps = new kakao.maps.services.Places();
+        ps.keywordSearch(searchInfo.search, placesSearchCB);
+    };
+    
     return (
         <>
             <div className="mainBox">
@@ -43,6 +73,10 @@ const Map = () => {
                 <button onClick={() => navigate('/notice')}>게시글 목록</button>
                 
                 <div>
+                    <form onSubmit={onSubmitHandler}>
+                        <input type="text" name="search" placeholder="search" onChange={onChangeHandler}/>
+                        <button type="submit">검색</button>
+                    </form>
                     <div
                         id="map"
                         ref={mapContainer}

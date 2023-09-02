056a410e9cbb571b2f03d483ec88b1613bdf8cd3
jia-son <116625758+jia-son@users.noreply.github.com>
2023-09-03 04:49:06 +0900

    react-calendar 연습
    day.js가 아니라 react-calendar 연습으로 변질되었다...

--- src/components/calendar/CalendarPrac.js
+++ src/components/calendar/CalendarPrac.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from "react-router-dom";
-import dayjs from 'dayjs';
+
 import '../main/main.css';
+
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css'; 
+import { format } from 'date-fns';
 
 const CalendarPrac = () => {
     const navigate = useNavigate();
+    const [value, onChange] = useState(new Date());
+
+    const [dateValue, setDateValue] = useState(value.toLocaleDateString());
+
+    useEffect(() => {
+        let date = value;
+        date.setHours(date.getHours() + 9);
+        const dateParts = date.toISOString().split('T')[0];
+
+        setDateValue(dateParts);
+    }, [value, dateValue]);
     
     return (
         <>
@@ -12,6 +27,17 @@ const CalendarPrac = () => {
             <h1>캘린더 페이지지렁</h1>
             <button onClick={() => navigate('/')}>메인</button>
             <button onClick={() => navigate('/notice')}>게시글 목록</button>
+
+            <hr />
+
+            <div>
+                <Calendar
+                    onChange={onChange}
+                    formatDay={(local, date) => format(date, 'd')}
+                    calendarType="gregory"
+                    value={value}
+                />
+            </div>
         </div>
         </>
     );
